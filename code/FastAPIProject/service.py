5f70d55c7661614acbe3db777cfd49f806751f4d
subhankar <sub.nitt@gmail.com>
2025-03-25 15:35:27 +0530

    updated service to send only detected anomalies

--- code/FastAPIProject/service.py
+++ code/FastAPIProject/service.py
@@ -7,10 +7,12 @@ from model.autoencoder_model.autoencoder import Autoencoder
 
 def train(file_path="data/train_data.csv"):
     pass
-def __save_file(data_frame,anomaly_score,file_path):
+def __save_file(data_frame,anomaly_score):
     buffer = io.StringIO()
-    data_frame['ANOMALY'] = anomaly_score
-    data_frame.to_csv(buffer,index=False)
+    key = "ANOMALY"
+    data_frame[key] = anomaly_score
+    df_filtered =data_frame [data_frame[key] != 0]
+    df_filtered.to_csv(buffer,index=False)
     buffer.seek(0)
     return buffer
 
@@ -20,6 +22,6 @@ def predict(file_path):
     x_test,y_test = data_processing.process_data_cleanup(columns=['As of Date', 'Primary Account', 'Secondary Account','Comments','ANOMALY'])
     model = Autoencoder(model=None)
     anomaly_col = model.predict(x_test,y_test)
-    csv_value = __save_file(df,anomaly_col,file_path)
+    csv_value = __save_file(df,anomaly_col)
     return csv_value
 
